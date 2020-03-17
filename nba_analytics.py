5353d7143e91bcbd0145dda80b50afac1e1db21e
oppong <michaeloppong123@gmail.com>
2020-03-16 23:14:22 -0400

    making sure everything is installed properly, and understanding how the basket-ball-reference-webscraper works

--- nba_analytics.py
+++ nba_analytics.py
@@ -1,7 +1,26 @@
-import basketball_reference_web_scraper as ball
+from basketball_reference_web_scraper import client
+import basketball_reference_web_scraper.data as data
+from pprint import pprint
 
+from pprint import pprint
+from pandas import DataFrame
 
-print("y")
+def main():
+    df = DataFrame()
+    columns = []
+    for x in client.players_advanced_season_totals(2020)[0]:
+        columns.append(x)
+
+    df.rename(columns = columns)
+    for x in (client.players_advanced_season_totals(season_end_year=2020)):
+        for y in dict(x):
+            print (y.value)
+            pprint("asa")
+
+    
+
+if __name__ == "__main__":                     
+    main()
 
 
 
